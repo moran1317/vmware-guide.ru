a18cca8790c8a48d1b8f8d5101684292a01fa36d
av.kuldyrkaev <moran1317@yandex.ru>
2023-02-17 14:55:01 +0300

    17.02-14:46 avto zapolnenie

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,14 +4,33 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
+
 
 class DatabaseSeeder extends Seeder
 {
     /**
      * Seed the application's database.
+     *
+     * @return void
      */
     public function run()
     {
 
+        DB::table('authors')->insert([
+            'full_name' => Str::random(10),
+        ]);
+        DB::table('categories')->insert([
+            'name' => Str::random(10),
+        ]);
+
+
+        // \App\Models\User::factory(10)->create();
+
+        // \App\Models\User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
     }
 }
